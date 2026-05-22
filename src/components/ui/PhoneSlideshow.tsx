bfbce7e03481a8049b2a6e5874db4c5580a692a9
Moritz Hutzler <moritz.hutzler@gmail.com>
2026-05-22 07:59:32 +0000

    Phone-Border off-white + Yellow-Block auf 800px Breite (= Hero)

--- src/components/ui/PhoneSlideshow.tsx
+++ src/components/ui/PhoneSlideshow.tsx
@@ -23,7 +23,7 @@ export function PhoneSlideshow({ slides, className = "", intervalMs = 3000 }: Pr
 
   return (
     <div className={className}>
-      <div className="relative aspect-[9/19] w-full overflow-hidden rounded-[28px] border-[5px] border-text-dark bg-white shadow-2xl">
+      <div className="relative aspect-[9/19] w-full overflow-hidden rounded-[28px] border-[5px] border-[#fafafa] bg-white shadow-2xl">
         {slides.map((slide, i) => (
           <div
             key={slide.src}
